73331b5d7bd6b8ab7ab1b35d54fb5ac9bf33171f
Steven Gonzales <stevendgonzales@gmail.com>
2013-09-06 11:21:32 -0500

    move db access moethods to model module

--- rift/data/model.py
+++ rift/data/model.py
@@ -1,5 +1,10 @@
 import uuid
 
+from rift.data.handler import get_handler
+from rift.data.model import build_job_from_dict
+
+JOB_COLLECTION = "jobs"
+
 
 class Tenant(object):
     def __init__(self, tenant_id, name=None, targets=None):
@@ -81,3 +86,36 @@ def _build_action_from_dict(action_dict):
 
 def _build_target_from_dict(target_dict):
     return Target(**target_dict)
+
+
+def save_job(job):
+    db_handler = get_handler()
+    db_handler.insert_document(
+        object_name=JOB_COLLECTION, document=job.as_dict()
+    )
+
+
+def update_job(job):
+    db_handler = get_handler()
+    db_handler.update_document(
+        object_name=JOB_COLLECTION,
+        document=job.as_dict(),
+        query_filter={"job_id": job.job_id}
+    )
+
+
+def get_job(job_id):
+    db_handler = get_handler()
+    job_dict = db_handler.get_document(
+        object_name=JOB_COLLECTION,
+        query_filter={"job_id": job_id})
+
+    return build_job_from_dict(job_dict)
+
+
+def delete_job(job_id):
+    db_handler = get_handler()
+    db_handler.delete_document(
+        object_name=JOB_COLLECTION,
+        query_filter={"job_id": job_id}
+    )

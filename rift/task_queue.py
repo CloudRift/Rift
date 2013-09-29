9ee7ff54257715403abea6ee790bfc9617e2bcf7
John Vrbanac <john.vrbanac@rackspace.com>
2013-09-29 00:45:09 -0500

    Woops! Got a little carried away in the renaming.

--- rift/task_queue.py
+++ rift/task_queue.py
@@ -7,7 +7,7 @@ conf = get_config()
 celery = Celery('cloudrift')
 
 #load celery configurations from rift config and apply them
-celery.conf.broker_url = conf.celery.broker_url
-celery.conf.celeryd_concurrency = conf.celery.celeryd_concurrency
-celery.conf.celery_task_serializer = conf.celery.celery_task_serializer
-celery.conf.celeryd_hijack_root_logger = conf.celery.celeryd_hijack_root_logger
+celery.conf.BROKER_URL = conf.celery.broker_url
+celery.conf.CELERYD_CONCURRENCY = conf.celery.celeryd_concurrency
+celery.conf.CELERY_TASK_SERIALIZER = conf.celery.celery_task_serializer
+celery.conf.CELERYD_HIJACK_ROOT_LOGGER = conf.celery.celeryd_hijack_root_logger

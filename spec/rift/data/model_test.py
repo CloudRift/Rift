907c8b611b7971270acef9c1bf3775e749aa6af4
Melissa Kam <melissa.kam@rackspace.com>
2014-01-20 23:44:33 -0600

    Converting unit tests to Specter

---
+++ spec/rift/data/model_test.py
@@ -0,0 +1,66 @@
+from specter import Spec, expect
+from rift.data.model import Job, Target, Action, Tenant
+from uuid import uuid4
+
+
+class TestingDataModel(Spec):
+
+    def _create_target(self):
+        return Target(name=str(uuid4()), type="cloud-server",
+                      address={"ipv4": "4", "ipv6": "6"},
+                      address_type="nova-name",
+                      authentication={
+                          "cloud_account": {
+                              "username": "your_username",
+                              "token": "your_api_token"
+                          }
+                      })
+
+    def test_job_as_dict(self):
+        target1 = self._create_target()
+        target2 = self._create_target()
+        targets = [target1, target2]
+
+        actions = [
+            Action(targets=targets, action_type="soft-reboot"),
+            Action(targets=targets, action_type="apache-restart")
+        ]
+
+        job = Job(
+            tenant_id="123",
+            name="do_stuff",
+            actions=actions,
+            job_id="12345"
+        )
+        job_dict = job.as_dict()
+
+        test_job = Job.build_job_from_dict(job_dict)
+        expect(job_dict).to.equal(test_job.as_dict())
+
+    def test_target_as_dict(self):
+        target = self._create_target()
+        target_dict = target.as_dict()
+
+        test_target = Target.build_target_from_dict(target_dict)
+        expect(target_dict).to.equal(test_target.as_dict())
+
+    def test_tenant_as_dict(self):
+        target1 = self._create_target()
+        target2 = self._create_target()
+        targets = [target1, target2]
+        tenant = Tenant(name=str(uuid4()), tenant_id=str(uuid4()),
+                        targets=targets)
+        tenant_dict = tenant.as_dict()
+
+        test_tenant = Tenant.build_tenant_from_dict(tenant_dict)
+        expect(tenant_dict).to.equal(test_tenant.as_dict())
+
+    def test_action_as_dict(self):
+        target1 = self._create_target()
+        target2 = self._create_target()
+        targets = [target1, target2]
+        action = Action(targets=targets, action_type="soft-reboot")
+        action_dict = action.as_dict()
+
+        test_action = Action.build_action_from_dict(action_dict)
+        expect(action_dict).to.equal(test_action.as_dict())

ce355501f67f818701bccd74a758fb57681607cd
John Vrbanac <john.vrbanac@linux.com>
2014-08-23 18:15:31 -0500

    Fixed multi indent in container.sh

--- bin/containers.sh
+++ bin/containers.sh
@@ -2,13 +2,15 @@
 
 start_containers()
 {
-	sudo docker run --name rift-db -p 27017:27017 -d mongo
-	sudo docker run --name rift-mq -p 5672:5672 -p 15672:15672 -e RABBITMQ_PASS="password" -d tutum/rabbitmq
+  sudo docker run --name rift-db -p 27017:27017 -d mongo
+  sudo docker run --name rift-mq -p 5672:5672 -p 15672:15672 -e RABBITMQ_PASS="password" -d tutum/rabbitmq
 }
 stop_containers()
 {
-	sudo docker kill rift-db
-	sudo docker kill rift-mq
+  sudo docker kill rift-db
+  sudo docker kill rift-mq
+  sudo docker rm rift-db
+  sudo docker rm rift-mq
 }
 
 case "$1" in

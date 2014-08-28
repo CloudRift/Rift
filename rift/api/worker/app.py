3c332982714d374992d9226060db9905992339e2
John Vrbanac <john.vrbanac@rackspace.com>
2014-08-28 14:10:04 -0500

    Turn on resource to query available actions on a worker

--- rift/api/worker/app.py
+++ rift/api/worker/app.py
@@ -16,21 +16,22 @@ limitations under the License.
 from multiprocessing import Process
 
 from rift.app import App
-from rift import task_queue
+from rift.api.worker.resources import AvailableActionsResource
+from rift import task_queue, actions
 
 
 class WorkerApp(App):
 
     def __init__(self):
         super(WorkerApp, self).__init__()
+        self.action_plugins = actions.ACTION_PLUGINS
 
-        # available_actions = AvailableActionsResource(ACTION_PLUGINS)
-        # self.add_route('/actions', available_actions)
+        available_actions = AvailableActionsResource(self.action_plugins)
+        self.add_route('/actions', available_actions)
 
-        # TODO: Add routes for all of the plugins
-        # for action in self.action_plugins:
-        #     route_name = '/actions/{name}'.format(name=action.get_name())
-        #     self.add_route(route_name, action)
+        for action in self.action_plugins:
+            route_name = '/actions/{name}'.format(name=action.get_name())
+            self.add_route(route_name, action)
 
 
 celery_proc = Process(target=task_queue.celery.worker_main)

93bc7772c884b02897943ab1ced5c89ae2968aa8
Melissa Kam <melissa.kam@rackspace.com>
2015-04-07 19:19:05 -0500

    Adding api and worker containers

--- bin/containers.sh
+++ bin/containers.sh
@@ -2,15 +2,15 @@
 
 start_containers()
 {
-  sudo docker run --name rift-db -p 27017:27017 -d mongo
-  sudo docker run --name rift-mq -p 5672:5672 -p 15672:15672 -e RABBITMQ_PASS="password" -d tutum/rabbitmq
+  docker run --name rift-db -p 127.0.0.1:27017:27017 -d mongo
+  docker run --name rift-mq -p 127.0.0.1:5672:5672 -p 15672:15672 -e RABBITMQ_PASS="password" -d tutum/rabbitmq
 }
 stop_containers()
 {
-  sudo docker kill rift-db
-  sudo docker kill rift-mq
-  sudo docker rm rift-db
-  sudo docker rm rift-mq
+  docker kill rift-db
+  docker kill rift-mq
+  docker rm rift-db
+  docker rm rift-mq
 }
 
 case "$1" in
@@ -20,7 +20,11 @@ case "$1" in
   stop)
     stop_containers
     ;;
+  restart)
+    stop_containers
+    start_containers
+    ;;
   *)
-    echo "Usage: start_containers.sh {start|stop}"
+    echo "Usage: containers.sh {start|stop|restart}"
     exit 1
 esac

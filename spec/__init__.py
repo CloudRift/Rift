8802e2231589d9a9c9bc82a3d92ce3082489f904
Paul Glass <paul.glass@rackspace.com>
2015-07-20 16:46:22 +0000

    Mock out load_secret_key in the unittests

--- spec/__init__.py
+++ spec/__init__.py
@@ -3,3 +3,8 @@
 import mongomock
 import pymongo
 pymongo.MongoClient = mongomock.MongoClient
+
+# mock out the load_secret_key function, which normally looks for a file
+from cryptography.fernet import Fernet
+import rift.data.common
+rift.data.common.load_secret_key = lambda: Fernet.generate_key()
